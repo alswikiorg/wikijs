ed14611c96ad861c90278c6baa4b5b278fdafedf
Craig Reyenga <craig.reyenga@gmail.com>
2025-03-26 11:57:28 -0400

    Use 'imagefig' markdown container when rendering pages

--- server/modules/rendering/markdown-core/renderer.js
+++ server/modules/rendering/markdown-core/renderer.js
@@ -1,6 +1,7 @@
 const md = require('markdown-it')
 const mdAttrs = require('markdown-it-attrs')
 const mdDecorate = require('markdown-it-decorate')
+const mdContainer = require('markdown-it-container')
 const _ = require('lodash')
 const underline = require('./underline')
 
@@ -44,6 +45,30 @@ module.exports = {
       allowedAttributes: ['id', 'class', 'target']
     })
     mkdown.use(mdDecorate)
+
+    mkdown.use(mdContainer, 'imagefig', {
+      validate: function(params) {
+        return params.trim().match(/^imagefig \[([\w/\\]+)\] \[(jpg|jpeg|gif|webp|png)\]$/)
+      },
+      render: (tokens, idx) => {
+        var m = tokens[idx].info.trim().match(/^imagefig \[([\w/\\]+)\] \[(jpg|jpeg|gif|webp|png)\]$/)
+
+        if (tokens[idx].nesting === 1) {
+          // opening tag
+          var text = [
+            `<figure class="alsfig">`,
+            `<a class="nolink" href="https://assets.alswiki.org/${m[1]}_1600x1200.${m[2]}">`,
+            `<img src="https://assets.alswiki.org/${m[1]}_300x200.${m[2]}" />`,
+            `</a>`,
+            `<figcaption>`
+          ].join('\n')
+          return text
+        } else {
+          // closing tag
+          return '</figcaption></figure>\n'
+        }
+      }
+    })
 
     for (let child of this.children) {
       const renderer = require(`../${_.kebabCase(child.key)}/renderer.js`)
